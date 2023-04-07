553d4ed70bba9b43ed119c2f3177b2070fd64b32
Junrey Lañas <jrydev404@gmail.com>
2023-04-08 06:13:51 +0800

    Added function selected items

--- izteem-gamehub/src/App.tsx
+++ izteem-gamehub/src/App.tsx
@@ -4,9 +4,18 @@ import Message from "./Message";
 
 function App() {
   const items = ["New york", "Philadelphia", "Chicago", "India", "Pakistan"];
+
+  const handleSelectedItem = (item: string) => {
+    console.log(item);
+  };
+
   return (
     <div className="App">
-      <ListGroup items={items} heading="Cities" />
+      <ListGroup
+        items={items}
+        heading="Cities"
+        onSelectedItem={handleSelectedItem}
+      />
     </div>
   );
 }
