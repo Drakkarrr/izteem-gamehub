425509534e517378d59270b878cdacc7c37e032c
Junrey Lañas <jrydev404@gmail.com>
2023-04-11 18:29:36 +0800

    added expenses list dynamically

--- izteem-gamehub/src/App.tsx
+++ izteem-gamehub/src/App.tsx
@@ -1,10 +1,22 @@
+import { useState } from "react";
 import "./App.css";
 import Form from "./components/Form";
+import ExpenseList from "./expense-tracker/components/ExpenseList";
 
 function App() {
+  const [expenses, setExpenses] = useState([
+    { id: 1, description: "Car Insurance", amount: 100, category: "Car" },
+    { id: 2, description: "Groceries", amount: 200, category: "Food" },
+    { id: 3, description: "Rent", amount: 300, category: "Home" },
+  ]);
+
+  const handleDelete = (id: number) => {
+    setExpenses(expenses.filter((expense) => expense.id !== id));
+  };
+
   return (
     <div className="App">
-      <Form />
+      <ExpenseList expenses={expenses} onDelete={handleDelete} />
     </div>
   );
 }
--- izteem-gamehub/src/components/Form.tsx
+++ izteem-gamehub/src/components/Form.tsx
@@ -16,7 +16,7 @@ const Form = () => {
   const {
     register,
     handleSubmit,
-    formState: { errors },
+    formState: { errors, isValid },
   } = useForm<FormData>({ resolver: zodResolver(schema) });
 
   const onSubmit = (data: FieldValues) => console.log(data);
@@ -49,7 +49,7 @@ const Form = () => {
           />
           {errors.age && <p className="text-danger">{errors.age.message}</p>}
         </div>
-        <button className="btn btn-primary" type="submit">
+        <button disabled={!isValid} className="btn btn-primary" type="submit">
           Submit
         </button>
       </form>
