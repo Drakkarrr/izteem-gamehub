defd03a9c61993d0c23d67dcef779a4e52e7d280
Junrey Lañas <jrydev404@gmail.com>
2023-04-11 17:54:41 +0800

    Added zod and resolvers

--- izteem-gamehub/src/components/Form.tsx
+++ izteem-gamehub/src/components/Form.tsx
@@ -1,17 +1,23 @@
 import { useForm } from "react-hook-form";
 import { FieldValues } from "react-hook-form/dist/types";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 
-interface FormData {
-  name: string;
-  age: number;
-}
+const schema = z.object({
+  name: z.string().min(3, "Name must be at least 3 characters!"),
+  age: z
+    .number({ invalid_type_error: "Age field is required!" })
+    .min(18, "You must be at least 18 years old"),
+});
+
+type FormData = z.infer<typeof schema>;
 
 const Form = () => {
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm<FormData>();
+  } = useForm<FormData>({ resolver: zodResolver(schema) });
 
   const onSubmit = (data: FieldValues) => console.log(data);
 
@@ -24,28 +30,24 @@ const Form = () => {
             Name
           </label>
           <input
-            {...register("name", { required: true, minLength: 3 })}
+            {...register("name")}
             className="form-control"
             type="text"
             id="name"
           />
-          {errors.name?.type === "required" && (
-            <p className="text-danger">The name field is required!</p>
-          )}
-          {errors.name?.type === "minLength" && (
-            <p className="text-danger">The name must be atleast 3 characters</p>
-          )}
+          {errors.name && <p className="text-danger">{errors.name.message}</p>}
         </div>
         <div className="mb-3">
           <label className="form-label" htmlFor="age">
             Age
           </label>
           <input
-            {...register("age")}
+            {...register("age", { valueAsNumber: true })}
             className="form-control"
             type="number"
             id="age"
           />
+          {errors.age && <p className="text-danger">{errors.age.message}</p>}
         </div>
         <button className="btn btn-primary" type="submit">
           Submit
