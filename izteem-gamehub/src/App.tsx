a023790de86306f241f94d06b152c1d62b4b0cc5
Junrey Lañas <jrydev404@gmail.com>
2023-04-11 18:47:32 +0800

    filter, delete and categorized expenses

--- izteem-gamehub/src/App.tsx
+++ izteem-gamehub/src/App.tsx
@@ -1,9 +1,10 @@
 import { useState } from "react";
 import "./App.css";
-import Form from "./components/Form";
+import ExpenseFilter from "./expense-tracker/components/ExpenseFilter";
 import ExpenseList from "./expense-tracker/components/ExpenseList";
 
 function App() {
+  const [selectedCategory, setSelectedCategory] = useState("");
   const [expenses, setExpenses] = useState([
     { id: 1, description: "Car Insurance", amount: 100, category: "Car" },
     { id: 2, description: "Groceries", amount: 200, category: "Food" },
@@ -14,9 +15,16 @@ function App() {
     setExpenses(expenses.filter((expense) => expense.id !== id));
   };
 
+  const visibleExpenses = selectedCategory
+    ? expenses.filter((expense) => expense.category === selectedCategory)
+    : expenses;
+
   return (
     <div className="App">
-      <ExpenseList expenses={expenses} onDelete={handleDelete} />
+      <div className="mb-3">
+        <ExpenseFilter onFilter={(category) => setSelectedCategory(category)} />
+      </div>
+      <ExpenseList expenses={visibleExpenses} onDelete={handleDelete} />
     </div>
   );
 }
