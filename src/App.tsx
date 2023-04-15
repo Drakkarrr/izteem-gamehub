acc9e3a1bdee22d3c7c5560ac00de7d50ed3d20f
Junrey Lañas <jrydev404@gmail.com>
2023-04-15 10:01:06 +0800

    install chakraui

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 import React from "react";
+import { Button, ButtonGroup } from "@chakra-ui/react";
 
 const App = () => {
-  return <h1>Hello world!</h1>;
+  return (
+    <>
+      <h1>Hello world!</h1>
+      <ButtonGroup>
+        <Button colorScheme="blue">Blue Button</Button>
+        <Button colorScheme="red">Red Button</Button>
+      </ButtonGroup>
+    </>
+  );
 };
 
 export default App;
