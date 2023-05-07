3b5c5c6158e334067ddc28fe597765f89ebe37da
Junrey Lañas <jrydev404@gmail.com>
2023-05-07 19:30:01 +0800

    fix bug to genre

--- src/hooks/useGenres.tsx
+++ src/hooks/useGenres.tsx
@@ -6,5 +6,5 @@ export interface Genre {
   image_background: string;
 }
 
-const useGenres = (selectedGenre: Genre) => useData<Genre>("/genres");
+const useGenres = () => useData<Genre>("/genres");
 export default useGenres;
