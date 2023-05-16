a9304af9b2e32cf3e359e01500c4cdd997e0841d
Junrey Lañas <jrydev404@gmail.com>
2023-05-17 03:07:51 +0800

    build search component

--- src/components/ColorModeSwitch.tsx
+++ src/components/ColorModeSwitch.tsx
@@ -10,7 +10,7 @@ const ColorModeSwitch = () => {
         isChecked={colorMode === "dark"}
         onChange={toggleColorMode}
       ></Switch>
-      <Text>Dark Mode</Text>
+      <Text whiteSpace="nowrap">Dark Mode</Text>
     </HStack>
   );
 };
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,12 +1,14 @@
 import { HStack, Img } from "@chakra-ui/react";
 import IzteemLogo from "../assets/izteem.png";
 import ColorModeSwitch from "./ColorModeSwitch";
+import SearchInput from "./SearchInput";
 
 const NavBar = () => {
   return (
     <>
-      <HStack justifyContent={"space-between"} padding="10px">
+      <HStack padding="10px">
         <Img src={IzteemLogo} boxSize="150px" alt="Logo" />
+        <SearchInput />
         <ColorModeSwitch />
       </HStack>
     </>
